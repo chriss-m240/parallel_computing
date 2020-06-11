8c119ba236060be96ca90da78baa23eff7487218
chris <camantillad@unal.edu.co>
2020-06-11 13:18:24 -0500

    script

--- lab4/camantillad_dsramirezc_dcalbarracinv/script.sh
+++ lab4/camantillad_dsramirezc_dcalbarracinv/script.sh
@@ -1,34 +1,34 @@
 > out.txt
-g++ blur-effect.cpp -Wall -pedantic -fopenmp -w
+mpicc blur-effect1.c -o blur-effect -lm -fopenmp -Wall -pedantic
 for((j=3;j<=15;j+=2))
 do
-	for ((i=1; i <= 16; i*=2))
+	for ((i=1; i <= 4; i*=2))
 		do
 			for ((k=1; k <= 10; k+=1))
 				do
-	  			TIME="$(/usr/bin/time -f '%e' ./a.out  720.jpg 720blur.jpg $j  $i 2>&1 )"
+	  			TIME="$(/usr/bin/time -f '%e' mpirun -np $i --hostfile /home/mpiuser/mpi-hosts ./blur-effect 720.jpg 720blur.jpg $j 2 2>&1 )"
 	  			echo "720.jpg kernel: $j threads: $i tiempo: $TIME" >> out.txt
 			done
 		done
 done
 for((j=3;j<=15;j+=2))
 do
-	for ((i=1; i <= 16; i*=2))
+	for ((i=1; i <= 4; i*=2))
 		do
 			for ((k=1; k <= 10; k+=1))
 				do
-	  			TIME="$(/usr/bin/time -f '%e' ./a.out  1080.jpg 1080blur.jpg $j  $i 2>&1 )"
+	  			TIME="$(/usr/bin/time -f '%e' mpirun -np $i --hostfile /home/mpiuser/mpi-hosts ./blur-effect 1080.jpg 1080blur.jpg $j 2 2>&1 )"
 	  			echo "1080.jpg kernel: $j threads: $i tiempo: $TIME" >> out.txt
 				done
 		done
 done
 for((j=3;j<=15;j+=2))
 do
-	for ((i=1; i <= 16; i*=2))
+	for ((i=1; i <= 4; i*=2))
 		do
 			for ((k=1; k <= 10; k+=1))
 				do
-	  			TIME="$(/usr/bin/time -f '%e' ./a.out  4k.jpg 4kblur.jpg $j  $i 2>&1 )"
+	  			TIME="$(/usr/bin/time -f '%e' mpirun -np $i --hostfile /home/mpiuser/mpi-hosts ./blur-effect 4k.jpg 4kblur.jpg $j 2 2>&1 )"
 	  			echo "4k.jpg kernel: $j threads: $i tiempo: $TIME" >> out.txt
 				done
 		done
